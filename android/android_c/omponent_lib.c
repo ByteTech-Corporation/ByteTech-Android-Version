d3fece7831fdc13711a866cfb7583a03865d4d4e
Boonk3312 <theboonk7@hotmail.com>
2024-03-31 16:57:09 -0700

    Create omponent_lib.c

---
+++ android/android_c/omponent_lib.c
@@ -0,0 +1,13 @@
+#include <library.c>
+#include <library.h>
+#include <iostream>
+
+void main() {
+    if (int = 0 > 1, int = 0 > 0) {
+        return 160;
+        break;
+    } else {
+        return 132;
+        continue;
+    }
+}
