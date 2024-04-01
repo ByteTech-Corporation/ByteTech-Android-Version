936acba6329c1d946bd3d336c8edd6d39348b871
Boonk3312 <theboonk7@hotmail.com>
2024-03-31 17:14:21 -0700

    Create AndroidX.cs

---
+++ android/androidx/AndroidX.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program() {
+    public static void main(string, args) {
+        group AndroidX() {
+            androidx.library => defineObject(static group AndroidX_Library) {
+                if (null = 0x0001) {
+                    break;
+                } else {
+                    continue;
+                }
+
+                define<Object( return 14 )>;
+                /*
+                 *       @param Object<?>         | 0x4E09A6B
+                 *       @param ObjectUse<?>      | 0x9101833
+                 *       @param ObjectGet<?>      | 0x0001845
+                 *       @param ObjectGetItem<?>  | 0x0049382
+                 *
+                 */
+            }
+        }
+    }
+}
