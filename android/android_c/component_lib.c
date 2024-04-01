88f9121f58e95b73c302a4f8458128798a145232
Boonk3312 <theboonk7@hotmail.com>
2024-03-31 17:01:24 -0700

    Update component_lib.c

--- android/android_c/component_lib.c
+++ android/android_c/component_lib.c
@@ -5,9 +5,11 @@
 void main() {
     if (int = 0 > 1, int = 0 > 0) {
         return 160;
+        printf("Math check detected invalid equation, breaking operation");
         break;
     } else {
         return 132;
         continue;
+        printf("Math check completed with no errors");
     }
 }
