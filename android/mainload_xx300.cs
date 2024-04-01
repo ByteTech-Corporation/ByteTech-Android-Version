e3c2676bc972360951f999f9b9748d795010a07d
Boonk3312 <theboonk7@hotmail.com>
2024-03-31 17:45:59 -0700

    Create mainload_xx300.cs

---
+++ android/mainload_xx300.cs
@@ -0,0 +1,12 @@
+void mainload_xx300 {
+    Lists<group mainload()> {
+        if (1 < 0, 0 > 0, 0 > 1) {
+            return null;
+            return 0;
+            break;
+        } else {
+            continue;
+            return 1;
+        }
+    }
+}
