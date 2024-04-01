7d4f681f9d733e6c607507d504de930e09b7445b
Boonk3312 <theboonk7@hotmail.com>
2024-03-31 19:57:14 -0700

    Create AbstractDatavector.java

---
+++ android/AbstractDatavector.java
@@ -0,0 +1,18 @@
+package android.abstractdatavector;
+
+import java.io.*;
+import java.*;
+
+public class AbstractDatavector {
+    public static void main(String, args[]) {
+        abstract static void Datavector(String, args[]) {
+            @Override
+            /*
+             * @param Datavector[int];
+             * @param Datavector.Data[char data = ""];
+             */
+
+            int datavector = String datavector = {library.Datavector(hex().getInput().getProc())}
+        }
+    }
+}
