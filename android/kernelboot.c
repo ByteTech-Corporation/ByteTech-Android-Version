57d7f433930301ad088d61f6e4581ea558d6855c
Boonk3312 <theboonk7@hotmail.com>
2024-04-02 19:39:59 -0700

    Create kernelboot.c

---
+++ android/kernelboot.c
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// Kernel booter/loader
+
+void kernelboot() {
+    if (NULL == true) {
+        for (NULL = true) {
+            break;
+        } else {
+            continue;
+        }
+        return 0x04;
+        printf("NULL");
+        break;
+    } else {
+         continue;
+    }
+
+    static void kernelboot_ff20c4995() {
+        @override
+        if (return = NULL) {
+            break;
+        } else {
+            continue;
+        }
+    }
+}
